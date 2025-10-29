1afe870c8b9cb712627117330a68c6c3a1df5820
Vedant Patil <Vedant.patil@nimapinfotech.com>
2025-10-29 16:01:07 +0530

    setTimeout questions

---
+++ 29th_Sept/settimeout.js
@@ -0,0 +1,34 @@
+// for (let i = 0; i < 5; i++) {
+//     setTimeout(() => console.log(i), 0);
+// }
+
+// const isDone = false
+
+// if(isDone){
+//     console.log("Working.........");
+//     setTimeout(()=>{
+//     console.log("Done");
+// }, 2000)
+// }else{
+//     console.log("Not done")
+// };
+
+
+// let j = 0;
+// for (j = 0; j < 5; j++) {
+//     setTimeout(() => console.log(j), 0);
+// }
+
+
+// for (var k = 0; k < 5; k++) {
+//     setTimeout(() => console.log(k), 0);
+// }
+
+
+// var k;
+// for (k = 0; k < 5; k++) {
+//     setTimeout(() => console.log(k), 0);
+// }
+
+
+
