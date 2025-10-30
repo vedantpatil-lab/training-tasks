1b9332fba31903f405550e9e1ed9fb7a786b4706
Vedant Patil <Vedant.patil@nimapinfotech.com>
2025-10-30 10:52:12 +0530

    types of functions

---
+++ 30th_Sept/functions.js
@@ -0,0 +1,25 @@
+// ----- Types of function ------
+
+// a) Named function
+// function addNumbers(a,b){
+//     return a + b;
+// };
+
+// console.log(addNumbers(1,2))
+
+
+// b) Anonymous function task
+// const myName = function(name){
+//     console.log(name);
+// };
+// myName("vedant")
+
+// (function () {
+//     const name = "vedant";
+//     const age = 22;
+//     const gender = "male"
+//     console.log(`My name is ${name}, my age: ${age} and gender is ${gender}`)
+// })();
+
+
+
